7e84723f4ae861025f0d5509f63cef63da2cbce7
Chris <mr.chrisnash@gmail.com>
2021-07-12 17:17:39 -0700

    added rainbow and greyscale functionality

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const container = document.querySelector('.container');
 const title = document.querySelector('.title');
 const grid = document.querySelector('.grid');
 const controls = document.querySelector('.controls');
-const clearBtn = document.querySelector("#clear").addEventListener("click", clearCells);
-const gridSizeBtn = document.querySelector("#grid-size").addEventListener("click", setGrid);
+
+const clearBtn = document.querySelector("#clear-btn").addEventListener("click", clearCells);
+const gridSizeBtn = document.querySelector("#grid-btn").addEventListener("click", setGrid);
+const blackHoverBtn = document.querySelector("#black-btn").addEventListener("click", setHoverBlack);
+const rainbowHoverBtn = document.querySelector("#rainbow-btn").addEventListener("click", setHoverRainbow);
+const greyScaleHoverBtn = document.querySelector("#grey-scale-btn").addEventListener("click", setHoverGreyScale);
 
 
-title.innerHTML = "<h1>Etch-A-Sketch</h1>";
 
 //create default grid
-makeGrid(12);
+makeGrid(16);
 
 
 //make grid function
@@ -28,7 +31,7 @@ function makeGrid(size){
         grid.appendChild(cell).className = "cell";
 
         //add listener to each cell
-        cell.addEventListener("mouseover", hover);
+        cell.addEventListener("mouseover", hoverBlack);
     }
 
 }
@@ -46,11 +49,9 @@ function clearGrid(){
 
 //// EVENT LISTENERS ////
 
-//cell hover event
-function hover(e){
-    
-    e.target.style.setProperty("background-color", "black")
-}
+
+
+
 
 
 
@@ -69,13 +70,90 @@ function clearCells(){
 //user set grid size
 function setGrid(){
 
-    const size = prompt("enter grid size", "4-100")
+    const size = prompt("enter grid size", "4-64")
     
-    if(size > 100 || size < 4){
-        alert('valid range is 4-100')
+    if(size > 64 || size < 4){
+        alert('valid range is 4-64')
         return;
     }
     
     makeGrid(size);
 
 }
+
+function setHoverBlack(){
+
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach(element => {
+        element.removeEventListener("mouseover", hoverRainbow);
+        element.removeEventListener("mouseover", hoverGreyScale);
+
+        element.addEventListener("mouseover", hoverBlack);
+    });
+
+}
+
+function hoverBlack(e){
+    
+    e.target.style.setProperty("background-color", "#000000")
+}
+
+
+function setHoverRainbow(){
+
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach(element => {
+        element.removeEventListener("mouseover", hoverBlack);
+        element.removeEventListener("mouseover", hoverGreyScale);
+
+        element.addEventListener("mouseover", hoverRainbow);
+    });
+
+}
+
+function hoverRainbow(e){
+    
+    const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+
+    e.target.style.setProperty("background-color", randomColor)
+}
+
+
+function setHoverGreyScale(){
+
+    const cells = document.querySelectorAll('.cell');
+    
+
+    cells.forEach(element => {
+        element.removeEventListener("mouseover", hoverBlack);
+        element.removeEventListener("mouseover", hoverRainbow);
+
+        //custom attribute to help darken bgcolor
+        element.setAttribute("data-red", "255");
+        element.setAttribute("data-green", "255");
+        element.setAttribute("data-blue", "255");
+
+        element.addEventListener("mouseover", hoverGreyScale);
+    });
+
+}
+
+
+
+function hoverGreyScale(e){
+
+    let red = e.target.dataset.red;
+    let green = e.target.dataset.green;
+    let blue = e.target.dataset.blue;
+
+    e.target.style.setProperty("background-color", `rgb(${red},${green},${blue})`);
+
+    e.target.dataset.red -= 50;
+    e.target.dataset.green -= 50;
+    e.target.dataset.blue -= 50;
+
+}
+
+
